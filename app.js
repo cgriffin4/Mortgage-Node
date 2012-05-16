2bc3ea7cbb9d8d991772e34228a07c5a00888aee
ide user ide_cgriffin4 <chris.sgriffin@gmail.com>
2012-05-16 13:01:27 +0000

    Push to heroku

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.configure(function(){
         auth.Google2({appId : '129675806980.apps.googleusercontent.com', appSecret: 'ca93uhyzKU0zhhF53Y9rK5nk', callback: 'http://mortgage-42.herokuapp.com/oauth2callback', requestEmailPermission: true})
         ], 
         trace: true, 
+        firstLoginHandler: firstLoginHandler,
         logoutHandler: require('connect-auth/lib/events').redirectOnLogout("/")}))
     .use(example_auth_middleware())
    .use('/logout', function(req, res, params) {
@@ -82,6 +83,7 @@ app.configure(function(){
         .run(function (err, m) {
             if( req.isAuthenticated() ) {
                 user = req.getAuthDetails().user;
+                console.log(req.getAuthDetails());
             } else {
                 var user = 'undefined';
             }
@@ -193,68 +195,13 @@ var Mortgage = new Schema({
   , Transaction     :  [Transactions]
 });
 
-var m = mongoose.model('Mortgage', Mortgage, 'mortgage');
-/*
-// Routes
-app.get('/', function (req, res) {
-    m.findOne({})
-        .run(function (err, m) {
-            if( req.isAuthenticated() ) {
-                console.log('there');
-            }
-            if (req.query["access_token"]) {
-                console.log('here');
-            }
-            var data = m.toObject();
-            
-            //Initialize Variables
-            data.Balance = 0;
-            data.InterestPaid = 0;
-            data.PrincipalPaid = 0;
-            data.InterestDaily = 0;
-            data.InterestUnpaid = 0;
-            data.LastPayment = data.OrginDate;
-            
-            //Sort the Transactions as Newest to Oldest
-            data.Transaction = _.sortBy(data.Transaction, 'Date').reverse();
-            
-            //Balances and amounts paid - everything from transactions
-            for ( var t in data.Transaction) {
-                data.Balance += parseFloat(data.Transaction[t].Principal);
-                data.InterestPaid += parseFloat(data.Transaction[t].Interest);
-                if (parseFloat(data.Transaction[t].Principal) < 0) data.PrincipalPaid += Math.abs(parseFloat(data.Transaction[t].Principal));
-                data.LastPayment = ( data.Transaction[t].Date > data.LastPayment ? data.Transaction[t].Date : data.LastPayment );
-                
-                //Format Money
-                data.Transaction[t].Principal = accounting.formatMoney(data.Transaction[t].Principal);
-                data.Transaction[t].Interest = accounting.formatMoney(data.Transaction[t].Interest);
-                data.Transaction[t].Amount = accounting.formatMoney(data.Transaction[t].Amount);
-                
-                //Format Date
-                data.Transaction[t].Date = new Date(data.Transaction[t].Date).toDateString();
-            }
-            
-            //Outstanding Interest -- This only works if we assume all unpaid interest was paid at last payment
-            data.InterestDaily = (( data.APY / 100 ) / 365 ) * data.Balance;
-            var today = new Date();
-            data.daysToPayInterestOn = days_between(today, data.LastPayment)-1; //The last payment would have paid the interest through that day. (bug: before 1st payment, you get 1 day free interest)
-            data.InterestUnpaid = (data.daysToPayInterestOn * data.InterestDaily);
-            
-            //Format Date
-            data.LastPayment = new Date(data.LastPayment).toDateString();
-                
-            //Format Money
-            data.OrginAmount = accounting.formatMoney(data.OrginAmount);
-            data.Balance = accounting.formatMoney(data.Balance);
-            data.InterestPaid = accounting.formatMoney(Math.abs(data.InterestPaid));
-            data.PrincipalPaid = accounting.formatMoney(data.PrincipalPaid);
-            data.InterestDaily = accounting.formatMoney(data.InterestDaily);
-            data.InterestUnpaid = accounting.formatMoney(data.InterestUnpaid);
-            
-            //Render
-            res.render('mortgage', { title: 'Mortgage' , mortgage : data });
-        });
+var Users = new Schema({
+    ID              :   { type: Number }
+    , Email         :   { type: String }
 });
-*/
+
+var m = mongoose.model('Mortgage', Mortgage, 'mortgage');
+//var u = mongoose.model('Mortgage', Users, 'users');
+
 app.listen(process.env.PORT || 8001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
